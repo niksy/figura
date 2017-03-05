9b31f0b6b62a616f97c6fcc6e3f62eab63606516
Ivan Nikolić <niksy5@gmail.com>
2017-03-05 21:42:19 +0100

    Change "concurrency" in Karma config

--- karma.conf.js
+++ karma.conf.js
@@ -112,8 +112,7 @@ module.exports = function ( config ) {
 		},
 		browsers: ['BS-Chrome', 'BS-Firefox', 'BS-IE9', 'BS-iOS 8.3', 'BS-Android 4.2'],
 		singleRun: true,
-		concurrency: Infinity,
-		browserDisconnectTolerance: 3
+		concurrency: 2
 	});
 
 };
